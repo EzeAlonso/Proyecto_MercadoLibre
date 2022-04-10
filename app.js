ee804c5fdf090b4176aae18c49c66e7b1d9d8e77
EzeAlonso <alonsoezequiel@live.com.ar>
2022-04-10 14:15:01 -0300

    actualizado

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en el puerto 3000")
-});
+app.listen(3000);
 
 app.use(express.static(path.join(__dirname, "public")));
 
