388847dd367b0451ddf71590ed0f75b218b3c1fa
EzeAlonso <alonsoezequiel@live.com.ar>
2022-04-17 20:00:48 -0300

    terminado

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.listen(3000);
+app.listen(3000, () =>
+console.log("Corriendo con éxito"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-//localhost:3000/
+//localhost:3000//
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
 });
