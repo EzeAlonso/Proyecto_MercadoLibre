ff1e8c5d0a13bd74c81cef0b3e513a7b9bce232b
EzeAlonso <alonsoezequiel@live.com.ar>
2022-04-17 21:02:10 -0300

    Creación archivo Procfile

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const PORT = 3000
 
-app.listen(3000, () =>
+app.listen(process.env.PORT || 3000, () =>
 console.log("Corriendo con éxito"));
 
 app.use(express.static(path.join(__dirname, "public")));
